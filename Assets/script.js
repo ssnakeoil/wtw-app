d02d7c9662e1148d14c9fe44d49c6f9265719ca2
Stefi <114958335+ssnakeoil@users.noreply.github.com>
2023-03-12 20:39:28 -0400

    adding third party weather API & key; establishing some html before we continue

---
+++ Assets/script.js
@@ -0,0 +1,5 @@
+var weatherAPIKey = "4830514ebb8ce3f6f8d880610c75ec0e"; // weather API key
+var weatherAPIUrl = "http://api.openweathermap.org/data/2.5/weather?"; // weather API url
+var searches = []; // array will store searches
+
+
